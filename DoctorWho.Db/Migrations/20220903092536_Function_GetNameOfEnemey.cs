372b9141a96d9d68c2b3392b4f96d1697928e881
noor848 <54281955+noor848@users.noreply.github.com>
2022-09-06 11:04:26 +0300

    Migrations to create the functions,sprocs and views

---
+++ DoctorWho.Db/Migrations/20220903092536_Function_GetNameOfEnemey.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Migrations
+{
+    public partial class Function_GetNameOfEnemey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE FUNCTION  GetEnemyname
+            (@id int)
+              RETURNS TABLE AS
+               RETURN
+           select * from tblEnemys where tblEnemys.Id = @id;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP FUNCTION IF EXISTS GetEnemyname");
+        }
+    }
+}
---
+++ DoctorWho.Db/ViewProceduresFunction.cs
@@ -0,0 +1,39 @@
+﻿using EFCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db
+{
+    public static  class ViewProceduresFunction
+    {
+        public static void GetEnemiesNameProcedure(ApplicationContext context)
+        {
+            var EnemyNameList = context.tblEnemys.FromSqlRaw("EXECUTE GetEnemiesName").ToList();
+            foreach (var enemy in EnemyNameList)
+            {
+                Console.Write($"Enemy's Name {enemy.Id}: {enemy.EnemyName}, ");
+            }
+            Console.WriteLine("");
+        }
+        public static void PrintDoctorsNamesView(ApplicationContext context)
+        {
+            var NameList = context.viewPrintDoctorsNames.ToList();
+            
+            foreach (var name in NameList)
+            {
+                Console.Write($"Doctor's Name {name.Id}: {name.DoctorName}, ");
+            }
+            Console.WriteLine("");
+
+        }
+        public static void GetEnemyName_Function(ApplicationContext context,int id)
+        {
+            var response = context.EnemyFunctions.FromSqlRaw($"select* from  GetEnemyname({id})").ToList().FirstOrDefault();
+            Console.WriteLine($"Enemy Name has Id ={id} is {response.EnemyName}");
+        }
+    }
+}
